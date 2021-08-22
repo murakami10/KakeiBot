ab147743157af274b9a74174b09f72b23f661120
kyoya <m.kyoya777@gmail.com>
2021-08-23 03:21:12 +0900

    listenAndServe

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
-
-	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	text := "John."
+	fmt.Fprint(w, "Hi there, I'm ", text)
+}
 
 func main() {
 
-	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("ACCESS_TOKEN"))
+	//bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("ACCESS_TOKEN"))
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//message := linebot.NewTextMessage("hello, world")
+	//
+	//if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+	//	log.Fatal()
+	//}
 
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	message := linebot.NewTextMessage("hello, world")
-
-	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-		log.Fatal()
-	}
-
 }
