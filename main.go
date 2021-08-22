dacb6030d204d9ae3802a3d063b60af49f4348d7
kyoya <m.kyoya777@gmail.com>
2021-08-23 03:39:53 +0900

    メーセージを送るとmessageが帰ってくる

--- main.go
+++ main.go
@@ -5,26 +5,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
+	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("ACCESS_TOKEN"))
+
+	if err != nil {
+		log.Fatal(bot, err)
+	}
+
+	message := linebot.NewTextMessage("hello, world")
+
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+
 	text := "John."
 	fmt.Fprint(w, "Hi there, I'm ", text)
 }
 
 func main() {
-
-	//bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("ACCESS_TOKEN"))
-	//
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//message := linebot.NewTextMessage("hello, world")
-	//
-	//if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-	//	log.Fatal()
-	//}
 
 	port := os.Getenv("PORT")
 
